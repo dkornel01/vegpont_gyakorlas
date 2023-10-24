27cbecf2f735957d9456c0181301ce91112495e6
dkornel01 <kornel.dobrocsi@gmail.com>
2023-10-24 11:41:22 +0200

    tablazat fuggolegesen

--- MVC/View/Adatview.js
+++ MVC/View/Adatview.js
@@ -11,23 +11,26 @@ class Adatview{
 
     tablazatKiiro(){
         let kiir=""
-        kiir +=`<table>
+        kiir +=`<table class="tablazat">
         `
         let tarto=Object.keys(this.#lista[0])
         for (let index = 0; index < tarto.length; index++) {
                 kiir+=`<tbody>`
                 kiir+=`<tr>`
                 kiir+=`<th>${tarto[index]}</th>`
+                kiir+=`</tr>`
                 let tarto2=Object.values(this.#lista)
+                
                 for (let x = 0; x < tarto2.length; x++) {
+                kiir+=`<tr>`
                 let element=Object.values(tarto2[x])
                     kiir +=`
                     <td>${element[index]}</td>
                     `
                     console.log(element[index])
-                    
+                    kiir+=`</tr>`
                 }
-                kiir+=`</tr>`
+                
                 kiir+=`</tbody>`
             }
         kiir +=`
